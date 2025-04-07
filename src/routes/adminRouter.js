1b3524ca1b6b81abb50e1b4f9bef5ddf6beb6980
Jesus L. <jesusliracl@gmail.com>
2025-04-07 18:40:32 -0400

    Add tables in inicioAdmin

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -3,12 +3,33 @@ const adminRouter = express.Router();
 const { isAdmin } = require('../middlewares/authMiddleware');
 
 adminRouter.get('/inicio', isAdmin, (req, res) => {
-    res.render('pages/inicio', {
+    res.render('pages/admin/inicioAdmin', {
         progress: 65,
         titleBar: "Cargues del día",
         layout: 'main',
         tittle: 'Inicio',
-        user: req.user
+        user: req.user,
+        currentData: {
+            headers: ["Nombre", "Edad", "Email", "Ciudad"],
+            rows: [
+                ["Juan Pérez", 28, "juan@example.com", "Madrid"],
+                ["María García", 34, "maria@example.com", "Barcelona"],
+                ["Carlos López", 22, "carlos@example.com", "Valencia"],
+                ["Ana Martínez", 45, "ana@example.com", "Sevilla"],
+                ["Carlos López", 22, "carlos@example.com", "Valencia"],
+            ]
+        },
+        nextData: {
+            headers: ["Nombre", "Edad", "Email", "Ciudad"],
+            rows: [
+                ["Juan Pérez", 28, "juan@example.com", "Madrid"],
+                ["María García", 34, "maria@example.com", "Barcelona"],
+                ["Carlos López", 22, "carlos@example.com", "Valencia"],
+                ["Ana Martínez", 45, "ana@example.com", "Sevilla"],
+                ["Carlos López", 22, "carlos@example.com", "Valencia"],
+            ]
+        },
+
     });
 });
 
